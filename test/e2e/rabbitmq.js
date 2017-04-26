26b58a027ef181dfd01dd089a2d5f18200282662
Myztiq <kahn@runnable.com>
2017-04-26 16:39:38 -0700

    Added e2e tests

---
+++ test/e2e/rabbitmq.js
@@ -0,0 +1,33 @@
+const Publisher = require('ponos/lib/rabbitmq')
+const log = require('../../lib/logger').child({ module: 'e2e-tests' })
+const joi = require('joi')
+const Promise = require('bluebird')
+
+const getConnection = () => {
+  const publisher = new Publisher({
+    name: process.env.APP_NAME,
+    log: log,
+    hostname: process.env.RABBITMQ_HOSTNAME,
+    port: process.env.RABBITMQ_PORT,
+    username: process.env.RABBITMQ_USERNAME,
+    password: process.env.RABBITMQ_PASSWORD,
+    events: [{
+      name: 'org.registry.password.submitted',
+      jobSchema: require('../../lib/workers/org.registry.password.submitted').jobSchema
+    }]
+  })
+
+  return publisher.connect()
+    .return(publisher)
+    .disposer((connection) => {
+      return connection.disconnect()
+    })
+}
+
+module.exports = {
+  publishEvent: (eventName, data) => {
+    return Promise.using(getConnection(), (connection) => {
+      connection.publishEvent(eventName, data)
+    })
+  }
+}
